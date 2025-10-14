cc407639da6a2980980dc379b3eedc5b2e96faac
Lucía <lucyallassia12@gmail.com>
2025-10-13 23:26:47 -0300

    Clase Auto añadida (ejercicio 3)

--- auto.py
+++ auto.py
@@ -1,6 +1,38 @@
-import vehiculo
+# Ejercicio 3
 
+import vehiculo
 
 class Auto(vehiculo.Vehiculo):
 
-    pass
+    def __init__(self, numero_id, marca, modelo, anio, sucursal_id, estado_id, airbags, frenos_abs, caballos_fuerza):
+        super().__init__(numero_id, marca, modelo, anio, sucursal_id, estado_id)
+
+        self.__airbags = airbags
+        self.__frenos_abs = frenos_abs
+        self.__caballos_fuerza = caballos_fuerza
+
+    def __eq__(self, otro_auto):
+        pass
+
+    def __str__(self):
+        pass
+
+    def establecer_airbags(self, airbags):
+        self.__airbags = airbags
+
+    def establecer_frenos_abs(self, frenos_abs):
+        self.__frenos_abs = frenos_abs
+
+    def establecer_caballos_fuerza(self, caballos_fuerza):
+        self.__caballos_fuerza = caballos_fuerza
+
+    def obtener_airbags(self):
+        return self.__airbags
+    
+    def obtener_frenos_abs(self):
+        return self.__frenos_abs
+    
+    def obtener_caballos_fuerza(self):
+        return self.__caballos_fuerza
+
+
